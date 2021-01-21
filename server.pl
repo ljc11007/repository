0e24c92e30d8ba45763ce934b8b8e627896fe87e
ljc11007 <ljc11007@hotmail.com>
2021-01-21 19:01:43 +0800

    others

---
+++ server.pl
@@ -0,0 +1,32 @@
+#!/usr/bin/perl -w
+# Filename : server.pl
+use strict;
+use Socket;
+
+# 使用端口 7890 作为默认值
+my $port = shift || 7890;
+my $proto = getprotobyname('tcp');
+my $server = "localhost";  # 设置本地地址
+ 
+# 创建 socket, 端口可重复使用，创建多个连接
+socket(SOCKET, PF_INET, SOCK_STREAM, $proto)
+   or die "cannot open socket $!\n";
+setsockopt(SOCKET, SOL_SOCKET, SO_REUSEADDR, 1)
+   or die "can not SO_REUSEADDR $!\n";
+ 
+# 绑定端口并监听
+bind( SOCKET, pack_sockaddr_in($port, inet_aton($server)))
+   or die "cannot bind $port! \n";
+ 
+listen(SOCKET, 5) or die "listen: $!";
+print "start visit $port\n";
+ 
+# 接收请求
+my $client_addr;
+while ($client_addr = accept(NEW_SOCKET, SOCKET)) {
+   # send them a message, close connection
+   my $name = gethostbyaddr($client_addr, AF_INET );
+   print NEW_SOCKET "this message is coming from server";
+   print "Connection recieved from $name\n";
+   close NEW_SOCKET;
+}
